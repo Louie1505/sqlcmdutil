f3e43d5cddd3cbae5fec1979dd6feef7f875efd7
Louie Harrison <louie.harrison@cognisoft.co.uk>
2018-09-26 13:56:12 +0100

    Finished

--- RunSQL_CMD/Form1.Designer.cs
+++ RunSQL_CMD/Form1.Designer.cs
@@ -29,10 +29,12 @@
         private void InitializeComponent()
         {
             this.txtInstanceName = new System.Windows.Forms.TextBox();
-            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.btnBrowse = new System.Windows.Forms.Button();
             this.txtFilePath = new System.Windows.Forms.TextBox();
             this.btnGo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.SuspendLayout();
             // 
             // txtInstanceName
@@ -50,10 +52,10 @@
             this.btnBrowse.TabIndex = 1;
             this.btnBrowse.Text = "Browse";
             this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
             // 
             // txtFilePath
             // 
-            this.txtFilePath.Enabled = false;
             this.txtFilePath.Location = new System.Drawing.Point(197, 28);
             this.txtFilePath.Name = "txtFilePath";
             this.txtFilePath.Size = new System.Drawing.Size(167, 20);
@@ -61,19 +63,43 @@
             // 
             // btnGo
             // 
-            this.btnGo.Enabled = false;
             this.btnGo.Location = new System.Drawing.Point(135, 100);
             this.btnGo.Name = "btnGo";
             this.btnGo.Size = new System.Drawing.Size(92, 48);
             this.btnGo.TabIndex = 3;
             this.btnGo.Text = "Run SQL";
             this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Instance Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(194, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "SQL File Location";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(376, 172);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnGo);
             this.Controls.Add(this.txtFilePath);
             this.Controls.Add(this.btnBrowse);
@@ -88,10 +114,12 @@
         #endregion
 
         private System.Windows.Forms.TextBox txtInstanceName;
-        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.Button btnBrowse;
         private System.Windows.Forms.TextBox txtFilePath;
         private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- RunSQL_CMD/Form1.cs
+++ RunSQL_CMD/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace RunSQL_CMD
 {
@@ -15,6 +16,38 @@ namespace RunSQL_CMD
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtFilePath.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            string sSQLFilePath = string.Empty;
+            if (txtFilePath.Text.Length > 0)
+            {
+                sSQLFilePath = txtFilePath.Text;
+                if (txtInstanceName.Text.Length > 0)
+                {
+                    string sInstanceName = txtInstanceName.Text;
+                    if (Path.GetExtension(sSQLFilePath).ToLower() == ".sql")
+                    {
+                        string sCommand = "/C sqlcmd -S " + sInstanceName.Trim() + " -i \"" + sSQLFilePath + "\"";
+                        System.Diagnostics.Process.Start("CMD.exe", sCommand);
+                    }
+                    else
+                        MessageBox.Show("Only SQL files can be run, please choose a valid SQL file.");
+                }
+                else
+                    MessageBox.Show("Please enter a valid instance name.");
+            }
+            else
+                MessageBox.Show("Please choose a file.");
         }
     }
 }
